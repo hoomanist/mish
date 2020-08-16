95226e7a6343e19ef8c7ecad1733b1ce4d0e9b51
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2020-08-16 21:51:38 +0430

    docs

--- main.c
+++ main.c
@@ -14,16 +14,28 @@
 
 #define BUFFER 64
 
+
+/* split a command into tokens
+*/
 char **split(char *line) {
+
+  // split input by newline
   line = strtok(line, "\n");
   int buff = BUFFER;
+
+  // tokens will be stored here
   char **tokens = malloc(buff * sizeof(char*));
   char *token;
+
   if (tokens == NULL) {
     printf("mish: allocation error\n");
     exit(1);
   }
+
+  // token index counter
   int i = 0;
+
+  // split line by whitespace
   token = strtok(line, " "); 
   while (token != NULL) { 
       tokens[i] = token;
@@ -32,54 +44,108 @@ char **split(char *line) {
 
   } 
   tokens[i] = NULL;
+
   return tokens;
 }
 
+/* execute a command.
+command is a string array with each argument in a seperate element.
+*/
 void command(char **arguments){
+
+  // check if command is not empty
   if(arguments[0])
   {
+
+    // if command was 'cd', working directory should be changed.
     if(! strcmp(arguments[0], "cd")){
+
+      // if command has got no arguments,
+      // working directory should be changed to user home.
       if(!arguments[1]){
         chdir("~");
+
+      // but if it has an argument,
       }else{
+
+        // change working directory.
         if(chdir(arguments[1])){
+
+          // if address were given wrong
           if(errno == 2){
             printf("directory not found \n");
           }
         }
       }
     }
+
+    // if command is 'exit' shell should be terminated.
     else if(! strcmp(arguments[0], "exit")){
+
+      // terminate the shell
       exit(0);
     }
+
+    // if command is 'clear', screen should be cleared.
     else if(! strcmp(arguments[0], "clear")){
+
+      // clear the terminal
       printf("\033[2J\033[1;1H");
     }
   }
 }
+
+/* get a command from stdin and parse/execute it
+*/
 void prompt() {
+
   char input[CHAR_MAX];
   char identify[4];
   char **arguments;
   char cwd[CHAR_MAX];
+
+  // get working directory
+  // also check for errors
   if (!(getcwd(cwd, sizeof(cwd)) != NULL)) {
     perror("getcwd() error");
   }
+
+  // get user ID
   uid_t user = getuid();
+
+  // if user was logged in as root, show a '#' sign.
   if (user == 0) {
     strcpy(identify, "#");
+
+  // if user was logged in as normal user, show a '$' sign.
   } else {
     strcpy(identify, "$");
   }
+
+  // print out begining of command line
   printf("[%s]%s ", cwd, identify);
+
+  // get user command from stdin
   fgets(input, sizeof(input), stdin);
+
+  // split command arguments
   arguments = split(input);
+
+  // execute the command
   command(arguments);
+
   free(arguments);
 }
 
+/*
+  start of program.
+*/
 int main(int argc, char *argv[]) {
+
+  // hello message of the shell goes here
   printf("welcome to minimalistic shell ! \n");
+
+  // start getting commands
   for(;;){
     prompt();
   }
