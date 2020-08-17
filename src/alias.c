8850ec8d39108e7f99909d587eed27d8411c7274
hoomanist <hham.group12@gmail.com>
2020-08-17 19:47:01 +0430

    add docs

--- src/alias.c
+++ src/alias.c
@@ -35,9 +35,11 @@ char *alias_check(char *input){
     char *text;
     char *name;
     char *command;
+    // storing place
     char *filename = strcat(getenv("HOME"), "/.mish_alias");
 
     int i=0;
+    // open file
     fp = fopen(filename, "r");
     if(fp == NULL) {
         perror("Error in opening file");
@@ -50,14 +52,18 @@ char *alias_check(char *input){
         i++;
     } while(1);
     i = 0;
+    // seprate it by \n
     char line = strtok(text, "\n");
     while (line != NULL)
     {
+        // seprate by tabs
         char *token = strtok(line, "\t");
         while(token != NULL){
             if(i == 0){
+                // assigning name
                 name = token;
             }else if(i == 1){
+                // command ti run
                 command = token;
             }
             else{
@@ -65,12 +71,14 @@ char *alias_check(char *input){
             }
             token = strtok(NULL, "\t");
         }
+        // check if it is alias
         if(! strcmp(input, name))
             return command;
         else {
             continue;
         } 
     }
+    // if it is not alias
     return input;
 
 }
